8f94669a787a60719008dcfab0b07adac3b608e0
Grace Zhang <grace@honestbee.com>
2016-06-05 21:40:43 +0800

    Remove show new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -53,5 +53,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  resources :stores, :only [:index, :show, :new, :edit]
+  resources :stores, :only [:index]
 end
