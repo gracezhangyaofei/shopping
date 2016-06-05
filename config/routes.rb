118f9fdcad90c7518a4cc3699b089e6978c4eeef
Grace Zhang <grace@honestbee.com>
2016-06-05 21:55:55 +0800

    Add brand show route

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   resources :stores, :only [:index]
-  resources :brands, :only [:index]
+  resources :brands, :only [:index, :show]
 end
